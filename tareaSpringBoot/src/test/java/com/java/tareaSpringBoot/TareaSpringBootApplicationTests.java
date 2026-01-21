65ed51573732d77809d8213183a1ad9f2e804f87
unknown <sergio.garciapedrero7@gmail.com>
2026-01-21 14:05:40 +0100

    Meto otra carpeta generada por Springtools initializr

---
+++ tareaSpringBoot/src/test/java/com/java/tareaSpringBoot/TareaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.tareaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
